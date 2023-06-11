b1a9ab38b350fc7868725eb7d35419521ff4aaeb
Ryan Kruberg <rkruberg@gmail.com>
2023-06-11 21:36:51 +0300

    change: app layout

--- src/app.vue
+++ src/app.vue
@@ -1,66 +1,78 @@
 <template>
-  <v-card class="w-full h-full">
-    <v-layout class="w-full h-full">
-      <v-app-bar :elevation="2"
-        ><template v-slot:prepend>
-          <v-app-bar-nav-icon @click="showDrawer = !showDrawer"></v-app-bar-nav-icon> </template
-        ><template v-slot:append
-          ><div class="flex flex-row">
-            <v-select
-              v-model="$i18n.locale"
-              :items="$i18n.availableLocales"
-              variant="solo"
-              hide-details="auto"
-              density="compact"
-              ><template v-slot:selection="{ item }">
-                <div class="flex items-center">
-                  <Icon :icon="getLocaleMetaInfo(item.value).icon" class="mr-1"></Icon>
-                  <span>{{ getLocaleMetaInfo(item.value).label }}</span>
-                </div>
-              </template>
-              <template v-slot:item="{ item }">
-                <v-list-item @click="$i18n.locale = item.value">
-                  <v-list-item-title class="flex items-center"
-                    ><Icon :icon="getLocaleMetaInfo(item.value).icon" class="mr-1"></Icon
-                    >{{ getLocaleMetaInfo(item.value).label }}</v-list-item-title
-                  >
-                </v-list-item>
-              </template></v-select
-            >
-          </div></template
-        ></v-app-bar
-      >
+  <v-app>
+    <v-card class="w-full h-full">
+      <v-layout class="w-full h-full">
+        <v-app-bar :elevation="2"
+          ><template v-slot:prepend>
+            <v-app-bar-nav-icon @click="showDrawer = !showDrawer"></v-app-bar-nav-icon> </template
+          ><template v-slot:append
+            ><div class="flex flex-row">
+              <v-select
+                v-model="$i18n.locale"
+                :items="$i18n.availableLocales"
+                variant="solo"
+                hide-details="auto"
+                density="compact"
+                ><template v-slot:selection="{ item }">
+                  <div class="flex items-center">
+                    <Icon :icon="getLocaleMetaInfo(item.value).icon" class="mr-1"></Icon>
+                    <span>{{ getLocaleMetaInfo(item.value).label }}</span>
+                  </div>
+                </template>
+                <template v-slot:item="{ item }">
+                  <v-list-item @click="$i18n.locale = item.value">
+                    <v-list-item-title class="flex items-center"
+                      ><Icon :icon="getLocaleMetaInfo(item.value).icon" class="mr-1"></Icon
+                      >{{ getLocaleMetaInfo(item.value).label }}</v-list-item-title
+                    >
+                  </v-list-item>
+                </template></v-select
+              >
+            </div></template
+          ></v-app-bar
+        >
 
-      <v-navigation-drawer v-model="showDrawer" temporary>
-        <v-list-item
-          prepend-avatar="https://avatars.githubusercontent.com/u/49920260?v=4"
-          :title="store.dateNow.isBefore(weddingCeremonyDate) ? 'Ryan Kruberg' : 'Ryan Murulo'"
-          href="#/ryan"
-        ></v-list-item>
-        <v-list-item
-          prepend-avatar="https://media.licdn.com/dms/image/C4E03AQFTWNoK8uYYEw/profile-displayphoto-shrink_800_800/0/1627893179332?e=2147483647&v=beta&t=R1xpYiZE35TsgB2h9YSGzCzC4D8GCm3vWbiJCdrjnEM"
-          title="Marion Murulo"
-          href="#/marion"
-        ></v-list-item>
+        <v-navigation-drawer v-model="showDrawer" temporary>
+          <v-list-item
+            prepend-avatar="https://avatars.githubusercontent.com/u/49920260?v=4"
+            :title="store.dateNow.isBefore(weddingCeremonyDate) ? 'Ryan Kruberg' : 'Ryan Murulo'"
+            href="#/ryan"
+          ></v-list-item>
+          <v-list-item
+            prepend-avatar="https://media.licdn.com/dms/image/C4E03AQFTWNoK8uYYEw/profile-displayphoto-shrink_800_800/0/1627893179332?e=2147483647&v=beta&t=R1xpYiZE35TsgB2h9YSGzCzC4D8GCm3vWbiJCdrjnEM"
+            title="Marion Murulo"
+            href="#/marion"
+          ></v-list-item>
 
-        <v-divider class="border-opacity-75"></v-divider>
+          <v-divider class="border-opacity-75"></v-divider>
 
-        <v-list density="compact" nav>
-          <template v-for="route in routes" :key="route.route">
-            <v-list-item
-              v-if="route.type === 'nav'"
-              :key="route.route"
-              :prepend-icon="route.icon"
-              :title="t(route.translateKey)"
-              :href="`#${route.route}`"
-            ></v-list-item>
-            <v-divider v-if="route.type === 'divider'" class="border-opacity-75"></v-divider>
-          </template>
-        </v-list>
-      </v-navigation-drawer>
-      <v-main><component class="w-full h-full overflow-auto" :is="currentView.component" /></v-main>
-    </v-layout>
-  </v-card>
+          <v-list density="compact" nav>
+            <template v-for="route in routes" :key="route.route">
+              <v-list-item
+                v-if="route.type === 'nav'"
+                :key="route.route"
+                :prepend-icon="route.icon"
+                :title="t(route.translateKey)"
+                :href="`#${route.route}`"
+              ></v-list-item>
+              <v-divider v-if="route.type === 'divider'" class="border-opacity-75"></v-divider>
+            </template>
+          </v-list>
+        </v-navigation-drawer>
+
+        <v-main
+          ><component class="w-full h-full overflow-auto pb-4" :is="currentView.component"
+        /></v-main>
+      </v-layout>
+    </v-card>
+
+    <!-- Honestly, I don't see the point in a footer -->
+    <!-- <v-footer class="bg-grey-lighten-1">
+      <v-row justify="center" no-gutters>
+        {{ store.dateNow.toDate().toLocaleTimeString() }}
+      </v-row>
+    </v-footer> -->
+  </v-app>
 </template>
 
 <script setup lang="ts">
