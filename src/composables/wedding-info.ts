e48a445a9a45db8549f55c6ac69318c9f4987790
Ryan Kruberg <rkruberg@gmail.com>
2023-06-22 22:30:13 +0300

    refactor: extract url to object

--- src/composables/wedding-info.ts
+++ src/composables/wedding-info.ts
@@ -2,12 +2,15 @@ import type { MapLinksCSV, MapLinksParsed } from '@/_types/requests';
 import axios from 'axios';
 import { ref } from 'vue';
 
+const infoSourceURLs = {
+  mapLinks:
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2TZnFzaS96BT8muIZT3zM5CCQqZ3iQk3tzfqtUsobPYDG4QvM74N42tAQcYY2dgG7IF6V8WKUFPCL/pub?gid=0&single=true&output=csv',
+};
+
 export const useWeddingInfo = () => {
   const info = ref<MapLinksParsed>();
   const update = async () => {
-    const sourceUrl =
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2TZnFzaS96BT8muIZT3zM5CCQqZ3iQk3tzfqtUsobPYDG4QvM74N42tAQcYY2dgG7IF6V8WKUFPCL/pub?gid=0&single=true&output=csv';
-    const weddingInfoRequest = await axios.get<string>(sourceUrl);
+    const weddingInfoRequest = await axios.get<string>(infoSourceURLs.mapLinks);
 
     const result: { [header: string]: string } = {};
     const infoRows = weddingInfoRequest.data.split(/\r\n|\r|\n/);
