11a6b874f512ba74dd03f969ba7f1f7c0e9f0e5e
Ryan Kruberg <rkruberg@gmail.com>
2023-06-22 22:32:51 +0300

    refactor: extract map links into a function

--- src/composables/wedding-info.ts
+++ src/composables/wedding-info.ts
@@ -7,22 +7,26 @@ const infoSourceURLs = {
     'https://docs.google.com/spreadsheets/d/e/2PACX-1vT2TZnFzaS96BT8muIZT3zM5CCQqZ3iQk3tzfqtUsobPYDG4QvM74N42tAQcYY2dgG7IF6V8WKUFPCL/pub?gid=0&single=true&output=csv',
 };
 
+async function getMapLinks() {
+  const response = await axios.get<string>(infoSourceURLs.mapLinks);
+
+  const result: { [header: string]: string } = {};
+  const infoRows = response.data.split(/\r\n|\r|\n/);
+  for (const row of infoRows) {
+    const rowSplit = row.split(',') as MapLinksCSV;
+    const key = rowSplit[0];
+    const value = rowSplit[1];
+    result[key] = value;
+  }
+
+  console.info('Updated wedding info', result);
+  return result as unknown as MapLinksParsed;
+}
+
 export const useWeddingInfo = () => {
   const info = ref<MapLinksParsed>();
   const update = async () => {
-    const weddingInfoRequest = await axios.get<string>(infoSourceURLs.mapLinks);
-
-    const result: { [header: string]: string } = {};
-    const infoRows = weddingInfoRequest.data.split(/\r\n|\r|\n/);
-    for (const row of infoRows) {
-      const rowSplit = row.split(',') as MapLinksCSV;
-      const key = rowSplit[0];
-      const value = rowSplit[1];
-      result[key] = value;
-    }
-
-    console.info('Updated wedding info', result);
-    info.value = result as unknown as MapLinksParsed;
+    info.value = await getMapLinks();
   };
 
   update();
