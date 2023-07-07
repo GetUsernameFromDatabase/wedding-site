84002159796fb2568d47be5b4b8af6f2457a9986
Ryan Kruberg <rkruberg@gmail.com>
2023-07-07 05:19:59 +0300

    feature: on-site avatar pictures

--- src/composables/people.ts
+++ src/composables/people.ts
@@ -3,6 +3,7 @@ import { useI18n } from 'vue-i18n';
 import { weddingCeremonyDate } from '@/info/dates';
 import type { useI18nType } from '@/plugins/i18n/vue-i18n';
 import { useMainStore } from '@/stores/main';
+import { basePath } from '@/config';
 
 interface PersonInfo {
   name: string;
@@ -21,15 +22,14 @@ export const useWeddingCouple = () => {
     description: t('ryan.description'),
     subtitle: t('ryan.profession'),
     href: '/ryan',
-    avatar: 'https://avatars.githubusercontent.com/u/49920260?v=4',
+    avatar: `${basePath}/ryan.png`,
   }));
   const marion = computed<PersonInfo>(() => ({
     name: 'Marion Murulo',
     description: t('marion.description'),
     subtitle: t('marion.profession'),
     href: '/marion',
-    avatar:
-      'https://media.licdn.com/dms/image/C4E03AQFTWNoK8uYYEw/profile-displayphoto-shrink_800_800/0/1627893179332?e=2147483647&v=beta&t=R1xpYiZE35TsgB2h9YSGzCzC4D8GCm3vWbiJCdrjnEM',
+    avatar: `${basePath}/marion.jpg`,
   }));
   return { ryan, marion };
 };
